7df61e8f4719900793d1f2a666301f20910ff8e2
Oscar Cheung <hycheung0509@gmail.com>
2015-05-15 15:16:21 +0800

    Use brewhome to install common softwares

--- install-deps.sh
+++ install-deps.sh
@@ -5,11 +5,5 @@ curl -L https://get.rvm.io | bash -s stable --ruby
 # Install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# https://github.com/rupa/z
-# z, oh how i love you
-cd ~/code
-git clone https://github.com/rupa/z.git
-chmod +x ~/code/z/z.sh
-# also consider moving over your current .z file if possible. it's painful to rebuild :)
-
-# z binary is already referenced from .bash_profile
+# Install common things
+brew install z bash-completion git coreutils vim
